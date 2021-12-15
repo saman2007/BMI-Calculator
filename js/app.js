d56091a9598652ab1e5682293c64738f7a9e7b37
Saman <91914392+saman2007@users.noreply.github.com>
2021-12-15 12:09:44 +0330

    update displaying section codes

--- js/app.js
+++ js/app.js
@@ -23,46 +23,52 @@ document.addEventListener("DOMContentLoaded", () => {
 /* a function to display height, weight, situation,
 BMI, healty weight range and etc on the modal card*/
 function displayBMI() {
-    document.querySelector(".loading").style.display = "flex";
-    //display loading gif for about 1 second
-    setTimeout(function() {
-        document.querySelector(".loading").style.display = "none";
-    }, 1500);
-    //display values after the loading gif
-    setTimeout(function() {
-        //check the values
-        if(heightInput.value != "" && weightInput.value != "") {
-            //creat an instance with users entered values
-            BMI = new CalculateBMI(Number(heightInput.value),
-            Number(weightInput.value));
-            //check calculate returned values
-                if (BMI.calculate() == "your height and weight is not in range." || BMI.calculate() == "your height is not in range." || BMI.calculate() == "your weight is not in range.") {
-                    document.querySelector(".infos-container").style.display = "none";
-                    document.querySelector(".errorBase").style.display = "flex";
-                    document.querySelector(".error").textContent = BMI.calculate();  
-                    openModal();                  
-                } else {
-                    //display the result
-                    document.querySelector(".infos-container").style.display = "block";
-                    document.querySelector(".errorBase").style.display = "none";
-                    document.querySelectorAll(".weight").forEach(element => {
-                        element.textContent = BMI.weight + "KG";
-                    });
-                    document.querySelectorAll(".height").forEach(element => {
-                        element.textContent = BMI.height + "CM";
-                    });
-                    document.querySelector(".BMI .info .number").textContent = BMI.calculate();
-                    document.querySelector(".BMI .situation span").textContent = BMI.situation();
-                    openModal();
-                }
+  document.querySelector(".loading").style.display = "flex";
+  //display loading gif for about 1 second and then show the values
+  setTimeout(function () {
+    document.querySelector(".loading").style.display = "none";
+      //check the values
+      if (heightInput.value != "" && weightInput.value != "") {
+        //creat an instance with users entered values
+        BMI = new CalculateBMI(
+          Number(heightInput.value),
+          Number(weightInput.value)
+        );
+        //check calculate returned values
+        if (
+          BMI.calculate() == "your height and weight is not in range." ||
+          BMI.calculate() == "your height is not in range." ||
+          BMI.calculate() == "your weight is not in range."
+        ) {
+          document.querySelector(".infos-container").style.display = "none";
+          document.querySelector(".errorBase").style.display = "flex";
+          document.querySelector(".error").textContent = BMI.calculate();
+          openModal();
         } else {
-            //display error
-            document.querySelector(".infos-container").style.display = "none";
-            document.querySelector(".errorBase").style.display = "flex";
-            document.querySelector(".error").textContent = "you didnt enter anything or you entered some letters";  
-            openModal();
+          //display the result
+          document.querySelector(".infos-container").style.display = "block";
+          document.querySelector(".errorBase").style.display = "none";
+          document.querySelectorAll(".weight").forEach((element) => {
+            element.textContent = BMI.weight + "KG";
+          });
+          document.querySelectorAll(".height").forEach((element) => {
+            element.textContent = BMI.height + "CM";
+          });
+          document.querySelector(".BMI .info .number").textContent =
+            BMI.calculate();
+          document.querySelector(".BMI .situation span").textContent =
+            BMI.situation();
+          openModal();
         }
-    }, 1500);
+      } else {
+        //display error
+        document.querySelector(".infos-container").style.display = "none";
+        document.querySelector(".errorBase").style.display = "flex";
+        document.querySelector(".error").textContent =
+          "you didnt enter anything or you entered some letters";
+        openModal();
+      }
+  }, 1500);
 }
 
 // open modal
